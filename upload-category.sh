b4bf6f86bf3a484ff81042c85089b654cee8d653
Michael Yen <lvlichael8@gmail.com>
2018-07-10 03:59:35 -0700

    Add support for credentials.yml

--- upload-category.sh
+++ upload-category.sh
@@ -1,4 +1,13 @@
 category=$1
+
+if [ -e "$category" ]; then
+    echo "$category does not exist!"
+    exit
+fi
+
+# Export ENV variables
+export $(sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' credentials.yml)
+
 for problem in $category/*; do
     problem_name=$(basename -- "$problem")
     python lib/upload.py output/$problem_name
