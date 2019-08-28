03944156af8eef8089cba112d3cd9f42b85e1aaf
Michael Yen <lvlichael8@gmail.com>
2019-08-27 21:30:13 -0700

    Refactor spacing in parse.py

--- lib/scraper/parse.py
+++ lib/scraper/parse.py
@@ -10,34 +10,44 @@ def parse(raw_path, topic, output_path):
     for subtopic in os.listdir(raw_topic_path):
         print(subtopic)
         for file in os.listdir(os.path.join(raw_topic_path, subtopic)):
-            if file.endswith('.html'):
-                print(file)
-                with open(os.path.join(raw_topic_path, subtopic, file)) as f:
-                    soup = BeautifulSoup(f, 'html.parser')
-                for i in soup.findAll('pre'):
-                    if 'class' in i.attrs: # Check if tag object contains class attribute
-                        if 'python;' in i['class'] or 'python3;' in i['class']: # Check if class attribute has python string
-                            problem = file.replace(".html", "") 
-                            new_folder = os.path.join(output_path, topic, subtopic, problem)
-                            if not os.path.exists(new_folder): # If folder for problem doesn't already exist
-                                os.makedirs(new_folder) # creates parent directories if necessary
-                            new_file = os.path.join(new_folder, problem+".py")
-                            if os.path.isfile(new_file):
-                                o = open(os.path.join(new_folder, problem+"2.py"), "w")
-                            else:
-                                o = open(new_file, "w")
-                            o.write(i.string.encode("utf-8"))
-                            o.close()
+            if not file.endswith('.html'):
+                continue
 
-                            for tag in soup.findAll("div", class_="entry-content"):
-                                for p in tag.findAll("p"):
-                                    text = ""
-                                    text += tag.text
-                                    text +="\n"
-                                    o = open(os.path.join(new_folder, "description.txt"), "w")
-                                    o.write(text.encode("utf-8"))
-                                    o.close()
-                            files_parsed += 1
+            print(file)
+
+            with open(os.path.join(raw_topic_path, subtopic, file)) as f:
+                soup = BeautifulSoup(f, 'html.parser')
+
+            for i in soup.findAll('pre'):
+                if 'class' not in i.attrs: # Check if tag object contains class attribute
+                    continue
+
+                if 'python;' not in i['class'] and 'python3;' not in i['class']: # Check if class attribute has python string
+                    continue
+
+                problem = file.replace(".html", "") 
+                new_folder = os.path.join(output_path, topic, subtopic, problem)
+                if not os.path.exists(new_folder): # If folder for problem doesn't already exist
+                    os.makedirs(new_folder) # creates parent directories if necessary
+
+                new_file = os.path.join(new_folder, problem+".py")
+                if os.path.isfile(new_file):
+                    o = open(os.path.join(new_folder, problem+"2.py"), "w")
+                else:
+                    o = open(new_file, "w")
+                o.write(i.string.encode("utf-8"))
+                o.close()
+
+                for tag in soup.findAll("div", class_="entry-content"):
+                    for p in tag.findAll("p"):
+                        text = ""
+                        text += tag.text
+                        text +="\n"
+                        o = open(os.path.join(new_folder, "description.txt"), "w")
+                        o.write(text.encode("utf-8"))
+                        o.close()
+                files_parsed += 1
+
     print(files_parsed)
 
 parse('raw', 'strings', 'output')
