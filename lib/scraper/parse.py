c3adf93ce05499c4fb937c77c496ba33961f13aa
Michael Yen <lvlichael8@gmail.com>
2019-08-27 22:53:31 -0700

    Clean-up and Add comment to parse.py

--- lib/scraper/parse.py
+++ lib/scraper/parse.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import os
 
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 def parse(raw_path, topic, output_path):
+    """
+    Args:
+        raw_path: path to raw html files
+        topic: name of the topic
+        output_path: name of the folder to store generated files
+    """
+
     files_parsed = 0
     raw_topic_path = os.path.join(raw_path, topic)
     for subtopic in os.listdir(raw_topic_path):
@@ -35,6 +39,7 @@ def parse(raw_path, topic, output_path):
                     o = open(os.path.join(new_folder, problem+"2.py"), "w")
                 else:
                     o = open(new_file, "w")
+
                 o.write(i.string.encode("utf-8"))
                 o.close()
 
@@ -46,6 +51,7 @@ def parse(raw_path, topic, output_path):
                         o = open(os.path.join(new_folder, "description.txt"), "w")
                         o.write(text.encode("utf-8"))
                         o.close()
+
                 files_parsed += 1
 
     print(files_parsed)
