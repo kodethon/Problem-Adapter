1f60b803b585155fe2986354ef41f4cdcdae18b5
Michael Yen <lvlichael8@gmail.com>
2019-09-18 00:35:18 -0700

    Add migrate/assignment-metadata.sh

---
+++ bin/migrate/assignment-metadata.sh
@@ -0,0 +1,12 @@
+dist_path=$2
+for problem in $1/*; do
+    problem_name=$(basename -- "$problem")
+    if [ -e "$dist_path/$problem_name" ]; then
+        command="cp $problem/metadata.json $dist_path/$problem_name"
+        echo $command
+        $command
+    else
+        #echo "$dist_path/$problem_name does not exist..."
+        cp -r $problem $dist_path
+    fi
+done
